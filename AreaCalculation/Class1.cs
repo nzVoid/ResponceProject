21645724bf6fd7ead6c0bea8a3d906a8f2c5e2f3
favnry <favnry@POSTPC>
2022-04-02 23:22:22 +0300

    Добавьте файлы проекта.

---
+++ AreaCalculation/Class1.cs
@@ -0,0 +1,50 @@
+﻿namespace Calculation
+{
+    public class AreaCalculation
+    {
+        public static double circle(double r)
+        {
+            double area = Math.PI * Math.Pow(r, 2);
+            return area;
+        }
+        public static double triangle(double a, double b ,double c)
+        {
+            double p = (a + b + c)/2;
+            double area = Math.Sqrt(p * (p - a) * (p - b) * (p - c));
+            return area;
+        }
+
+        public static bool rightTriangle(double a, double b, double c)
+        {
+            bool response = false;
+            double hypot;
+            if (a > b)
+            {
+                hypot = a;
+                
+            }
+            else if (a>c)
+            {
+                hypot = a;
+            }
+            else if(b>c){
+                hypot = b;
+            }
+            else
+            {
+                hypot = c;
+            }
+            if(hypot == a)
+            {
+                if(Math.Pow(hypot, 2) == Math.Pow(b, 2) + Math.Pow(c, 2)) { response = true; }
+            }else if(hypot == b) {
+                if(Math.Pow(hypot, 2) == Math.Pow(a, 2) + Math.Pow(c, 2)) { response = true; }
+            }else if(hypot == c)
+            {
+                if(Math.Pow(hypot, 2) == Math.Pow(a, 2) + Math.Pow(b, 2)) { response = true; }
+            }
+            
+            return response;
+        }
+    }
+}
---
+++ ResponceProject/Program.cs
@@ -0,0 +1,43 @@
+﻿using Calculation;
+class Programm
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Calculation area");
+        Console.WriteLine("Choose a figure:");
+        Console.WriteLine("1)Circle");
+        Console.WriteLine("2)Triangle");
+        Console.Write("Enter number of figure: ");
+        int point = Convert.ToInt32(Console.ReadLine());
+        switch (point)
+        {
+            case 1:
+                Console.Clear();
+                Console.Write("Enter radius of circle: ");
+                double rad = Convert.ToDouble(Console.ReadLine());
+                var answer = AreaCalculation.circle(rad);
+                Console.WriteLine(answer);
+                break;
+            case 2:
+                Console.Clear();
+                Console.WriteLine("Enter side a,b,c");
+                Console.Write("Enter a: ");
+                double a = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Enter b: ");
+                double b = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Enter c: ");
+                double c = Convert.ToDouble(Console.ReadLine());
+                if(AreaCalculation.rightTriangle(a, b, c) == true)
+                {
+                    Console.WriteLine("Triangle is rectangular");
+                }
+                else
+                {
+                    Console.WriteLine("Triangle is not rectangular ");
+                }
+                double answ = AreaCalculation.triangle(a,b,c);
+                Console.WriteLine(Convert.ToString(answ));
+                break;
+        }
+    }
+}
